902669580158828fff5f552f870e230830ae410d
Tepen <elcabron.penyah@gmail.com>
2021-06-14 16:56:27 +0800

    add jobs controller & api routes

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,46 @@
+module Api::V1
+  class JobsController < ApplicationController
+    def index
+      @jobs = Job.all
+      render json: @jobs
+    end
+
+    def show
+      @job = Job.find(params[:id])
+      render json: @job
+    end
+
+    def create
+      @job = Job.new(job_params)
+
+      if @job.save
+        render json: @job, status: :created
+      else
+        render json: @job.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @job = Job.find(params[:id])
+
+      if @job.update(job_params)
+        render json: @job
+      else
+        render json: @job.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @job = Job.find(params[:id])
+      @job.destroy
+    end
+
+    #####
+    private
+    def job_params
+      params
+        .require(:job)
+        .permit(:company, :position, :description)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/jobs', to: 'jobs#index'
+      get '/job/:id', to: 'jobs#show'
+      post '/jobs', to: 'jobs#create'
+      patch '/job/:id', to: 'jobs#update'
+      delete '/job/:id', to: 'jobs#destroy'
+    end
+  end
 end
